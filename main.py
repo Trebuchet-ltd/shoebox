2d5b010c8148004640e7827acd2631153bc82a40
Rohit <tprohit9@gmail.com>
2021-08-12 16:34:21 +0530

    Kernel working

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+from scipy import signal
 
 try:
     import bpy
@@ -14,7 +15,7 @@ def remove_background(images, background):
     for image in images:
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         # image = cv2.subtract(image, background)
-        _, thresh = cv2.threshold(image, 1, 255, cv2.THRESH_BINARY)
+        _, thresh = cv2.threshold(image, 1, 1, cv2.THRESH_BINARY)
 
         contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -74,15 +75,8 @@ def carve_out(cube, img_side, img_top):
     return cube
 
 
-def draw_mesh(data_arr, size):
+def draw_mesh(vertex_dict, vertex_array):
     faces = []
-    vets = []
-
-    for i in range(size):
-        for j in range(size):
-            for k in range(size):
-                if data_arr[i, j, k]:
-                    vets.append([i, j, k])
 
     mesh = bpy.data.meshes.new("myBeautifulMesh")  # add the new mesh
     obj = bpy.data.objects.new(mesh.name, mesh)
@@ -90,12 +84,62 @@ def draw_mesh(data_arr, size):
     col.objects.link(obj)
     bpy.context.view_layer.objects.active = obj
 
-    mesh.from_pydata(vets, [], faces)
+    adders = {
+        # x
+        (0, 0): [0, 1, 0],
+        (0, 1): [0, 0, 1],
+        (0, 2): [0, 1, 1],
+        # y
+        (1, 0): [1, 0, 0],
+        (1, 1): [0, 0, 1],
+        (1, 2): [1, 0, 1],
+        # z
+        (2, 0): [0, 1, 0],
+        (2, 1): [1, 0, 0],
+        (2, 2): [1, 1, 0],
+    }
+
+    for key in vertex_dict:
+        for i in range(3):
+            square = [vertex_dict[key]]
+            for j in range(3):
+                adder = adders[(i, j)]
+                vert = vertex_dict.get((key[0] + adder[0], key[1] + adder[1], key[2] + adder[2]))
+
+                if vert:
+                    square.append(vert)
+            if len(square) == 4:
+                faces.append((square[0], square[1], square[3], square[2]))
+
+    print(len(faces))
+
+    mesh.from_pydata(vertex_array, [], faces)
 
     bpy.context.view_layer.objects.active = obj
     bpy.ops.object.modifier_add(type='SUBSURF')
     mat = bpy.data.materials.get("back")
     bpy.context.active_object.data.materials.append(mat)
+
+
+def get_edges(cube):
+    actives_dict = {}
+    actives_array = []
+
+    n = 2
+    kern = np.full((n + 1, n + 1, n + 1), -1)
+    kern[1, 1, 1] = 26
+
+    # 3d convolve
+    cube = signal.convolve(cube, kern, mode='same')
+
+    for layer in range(cube.shape[0]):
+        for row in range(cube.shape[1]):
+            for col in range(cube.shape[2]):
+                if cube[layer][row][col] > 0.01:
+                    actives_array.append((layer, row, col))
+                    actives_dict[(layer, row, col)] = len(actives_array) - 1
+
+    return actives_dict, actives_array
 
 
 def main():
@@ -105,15 +149,21 @@ def main():
 
     images = read_images(["data/front.png", "data/side.png", "data/top.png"])
     images = remove_background(images, background)
-
     images = align_images(images)
+
+    for image in images:
+        cv2.imshow(f"{image}", image)
+
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
     cube = np.tile(images[0], (SIZE[0], 1, 1))
     cube = carve_out(cube, images[1], images[2])
+    vertex_dict, vertex_array = get_edges(cube)
 
-    draw_mesh(cube, SIZE[0])
+    draw_mesh(vertex_dict, vertex_array)
 
 
 if __name__ == '__main__':
-    SIZE = (256, 256)
+    SIZE = (64, 64)
     main()
